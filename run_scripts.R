fa94ee412c35ae453a0caa3759980bbafc355c58
Nils Reimer <nils.nkr.reimer@gmail.com>
2021-04-21 14:50:14 -0700

    update run_scripts.R

--- run_scripts.R
+++ run_scripts.R
@@ -38,3 +38,40 @@ rm(list = ls())
 
   # Run preregistered robustness checks
   source("scripts/run_preregistered_robustness_checks.R")
+
+  # Run meta-regression moderator analyses
+  source("scripts/run_moderator_analyses.R")
+
+  # Run meta-regression tree moderator analyses
+  source("scripts/run_exploratory_moderator_analyses.R")
+  
+  # Run analyses with negative contact
+  source("scripts/run_analyses_with_negative_contact.R")
+
+  # Run analyses with ingroup contact
+  source("scripts/run_analyses_with_ingroup_contact.R")
+
+  # Run analyses to test and correct for publication bias
+  source("scripts/run_meta_bias_ingroup_contact.R")
+  
+
+# Visualize ---------------------------------------------------------------
+  
+  # Make figures
+  source("scripts/figure-1.R")
+  source("scripts/figure-2.R")
+  source("scripts/figure-3.R")
+  source("scripts/figure-4.R")
+  source("scripts/figure-5.R")
+  source("scripts/figure-6.R")
+  source("scripts/figure-7.R")
+  source("scripts/figure-8.R")
+
+
+# Combine -----------------------------------------------------------------
+
+  # Make manuscript
+  source("manuscript.Rmd")
+
+  # Make supplemental materials
+  source("supplemental-materials.Rmd")
